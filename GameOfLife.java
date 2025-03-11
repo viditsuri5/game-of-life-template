e7abcfa4c87314e134a6fba98548d9e668b9533f
viditsuri5 <viditsuri5@gmail.com>
2025-03-11 05:15:38 +0000

    changes

--- GameOfLife.java
+++ GameOfLife.java
@@ -16,4 +16,34 @@ public class GameOfLife {
             display();
         }
     }
+    public void step() {
+        int [][] nextGrid = new int[width][height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int liveCells = countLiveNeighbors(x, y);
+                if (grid[x][y] == 1) {
+                    nextGrid [x][y] = (liveCells == 2 || liveCells == 3) ? 1 : 0;
+                } else {
+                    nextGrid[x][y] = (liveCells == 3) ? 1 : 0;
+                }
+            }
+        }
+        grid = nextGrid;
+    }
+    private int countLiveNeighbors(intx, int y) {
+        int count = 0;
+        int[] offsetX = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] offsetY = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i=0; i < 8; i++;){
+            int newX = (x + offsetX[i] + width) % width;
+            int newY = (y + offsetY[i] + height) % height;
+            count += grid[newX][newY];
+        }
+        return count;
+    }
+    public void setCellState(int x, int y, int state) {
+        
+    }
 }

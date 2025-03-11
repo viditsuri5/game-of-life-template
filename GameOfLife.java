c47c725882c14348a880b464604e19cf95d61bdf
viditsuri5 <viditsuri5@gmail.com>
2025-03-11 05:58:10 +0000

    had to change code because nothing was compiling

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,49 +1,70 @@
-import java.util.Random;
-
 public class GameOfLife {
-    private int[][] grid;
-    private int width, height;
+    private final int width, height;
+    private int[][] board, nextBoard;
 
-    public GameOfLife(int w, int h) {
-        this.width = w;
-        this.height = h;
-        this.grid = new int[width][height];
+    public GameOfLife(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.board = new int[height][width];
+        this.nextBoard = new int[height][width];
     }
 
-    public void run(int generations) {
-        for int(i = 0; i < generations; i++) {
+    public void run(int steps) {
+        for (int i = 0; i < steps; i++) {
             step();
-            display();
         }
     }
-    public void step() {
-        int [][] nextGrid = new int[width][height];
 
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                int liveCells = countLiveNeighbors(x, y);
-                if (grid[x][y] == 1) {
-                    nextGrid [x][y] = (liveCells == 2 || liveCells == 3) ? 1 : 0;
+    public void step() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int neighbors = countNeighbors(x, y);
+                if (board[y][x] == 1) {
+                    nextBoard[y][x] = (neighbors == 2 || neighbors == 3) ? 1 : 0;
                 } else {
-                    nextGrid[x][y] = (liveCells == 3) ? 1 : 0;
+                    nextBoard[y][x] = (neighbors == 3) ? 1 : 0;
                 }
             }
         }
-        grid = nextGrid;
+        int[][] temp = board;
+        board = nextBoard;
+        nextBoard = temp;
     }
-    private int countLiveNeighbors(intx, int y) {
-        int count = 0;
-        int[] offsetX = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] offsetY = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-        for (int i=0; i < 8; i++;){
-            int newX = (x + offsetX[i] + width) % width;
-            int newY = (y + offsetY[i] + height) % height;
-            count += grid[newX][newY];
+    public int countNeighbors(int x, int y) {
+        int count = 0;
+        for (int dy = -1; dy <= 1; dy++) {
+            for (int dx = -1; dx <= 1; dx++) {
+                if (dx == 0 && dy == 0) continue;
+                if (get(x + dx, y + dy) == 1) count++;
+            }
         }
         return count;
     }
-    public void setCellState(int x, int y, int state) {
-        
+
+    public int get(int x, int y) {
+        int wrappedX = (x + width) % width;
+        int wrappedY = (y + height) % height;
+        return board[wrappedY][wrappedX];
+    }
+
+    public void set(int x, int y, int[][] data) {
+        for (int dy = 0; dy < data.length; dy++) {
+            for (int dx = 0; dx < data[dy].length; dx++) {
+                int newX = (x + dx) % width;
+                int newY = (y + dy) % height;
+                board[newY][newX] = data[dy][dx];
+            }
+        }
+    }
+
+    public void print() {
+        for (int[] row : board) {
+            for (int cell : row) {
+                System.out.print(cell == 1 ? "■ " : ". ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
